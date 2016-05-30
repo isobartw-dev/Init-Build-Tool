7239e48ea8538545c7bb28adbac607f7bc657055
betsu <bumpchama@gmail.com>
2016-05-30 19:15:09 +0800

    Change package code, Add README.md
    Change package code
        - InitBuildTool.py：更新版本、build system
        - InitBuildTool.sublime-settings：增加 build system 檔案路徑
        - Main.sublime-menu：修改資料夾名稱

--- InitBuildTool.py
+++ InitBuildTool.py
@@ -1,10 +1,12 @@
 import sublime, sublime_plugin
 import os
 import shutil
+import time
 
 setting = sublime.load_settings("InitBuildTool.sublime-settings")
-workflow_path = setting.get('workflow_package_path')
-workflow_files = setting.get('use_files')
+workflowPath = setting.get('workflowPackagePath')
+workflowFiles = setting.get('useFiles')
+buildSysem = setting.get('buildSystem')
 
 class InitBuildToolCommand(sublime_plugin.WindowCommand):
 	def run(self):
@@ -12,30 +14,106 @@ class InitBuildToolCommand(sublime_plugin.WindowCommand):
 		if self.window.folders():
 			folder = self.window.folders()
 		else:
-			folder = "請先建立project"
+			sublime.message_dialog("請先建立project")
 
 		def on_done(index):
 			if index == -1: return
 			select = folder[index]
 			# print(select)
-			copy_files(select)
-			 
+			exist = []
+			overwrite = 'none' # none all file system
+			for file in workflowFiles:
+				result = os.path.exists(os.path.abspath(os.path.join(select, file)))
+				exist.append(result)
+			with open (self.window.project_file_name(), 'r+') as project:
+					projectData = project.readlines()
+					exist.append('\t"build_systems":\n' in projectData)
+			while exist.count(True) == len(exist):
+				if sublime.yes_no_cancel_dialog('build tool 已建立\n是否要更新為最新版本', '全部更新', '指定更新') == sublime.DIALOG_NO:
+					if sublime.yes_no_cancel_dialog('請選擇要更新的項目', '更新 task 及 package.json', '更新 build system') == sublime.DIALOG_YES:
+						overwrite = 'file'
+					elif sublime.DIALOG_NO:
+						overwrite = 'system'
+				elif sublime.DIALOG_YES:
+					overwrite = 'all'
+				break
+			while overwrite == 'none' and exist.index(True) < len(exist) - 1:
+				if not sublime.ok_cancel_dialog('task 和 package 已建立\n是否需要更新為最新版本', '更新'):
+					overwrite = 'system'
+				break
+			while overwrite == 'none' and exist.index(True) == len(exist) - 1:
+				if not sublime.ok_cancel_dialog('build system 已建立\n是否需要更新為最新版本', '更新'):
+					overwrite = 'file'
+				break
+			
+			copyFiles(select, copied, overwrite)
 		self.window.show_quick_panel(folder, on_done)
 
-		# def build():
-		# 	print('build')
+		def copied(overwrite):
+			if overwrite == 'file':
+				print('成功複製檔案') 
+				return
+			print('成功複製檔案，偵測是否有 build system')
+			file = os.path.abspath(os.path.join(workflowPath, buildSystem))
+			with open (self.window.project_file_name(), 'r+') as project:
+				projectData = project.readlines()
+				if overwrite == 'system' or 'all':
+					newProjectData = ['{\n']
+					breakLine = projectData.index('\t],\n') + 1
+					dataLine = len(projectData)
+					for line in range(breakLine, dataLine):
+						newProjectData.append(projectData[line])
+					projectData = newProjectData
+				with open (file, 'r') as txt:
+					# print(projectData)
+					txtData = txt.readlines()
+					dataLine = len(txtData)
+					for line in range(0, dataLine):
+						projectData.insert(line + 1,txtData[line])
+					else:	
+						# print(projectData)
+						project.seek(0, 0)
+						project.truncate(0)
+						project.writelines(projectData)
 
-		def copy_files(dest_folder):
-			for file in workflow_files:
-				src_path = os.path.abspath(os.path.join(workflow_path, file))
-				dest_path = os.path.abspath(os.path.join(dest_folder, file))
-				# print (src_path, dest_path)
-				if os.path.isdir(src_path):
-					shutil.copytree(src_path, dest_path)
+		def getSize(path):
+			folderSize = 0
+			if os.path.isdir(path):
+				for (path, dirs, files) in os.walk(path):
+					for file in files:
+						filename = os.path.abspath(os.path.join(path, file))
+						folderSize += os.path.getsize(filename)
+					size = folderSize
+					# print(forder_size)
+			else:
+				fileSize = os.path.getsize(path)
+				size = fileSize
+				# print(fileSize)
+			return size
+
+		def copyFiles(destFolder, copied, overwrite):
+			if overwrite == "system":
+				copied(overwrite)
+				return
+			for file in workflowFiles:
+				srcPath = os.path.abspath(os.path.join(workflowPath, file))
+				destPath = os.path.abspath(os.path.join(destFolder, file))
+				srcSize = getSize(srcPath)
+				# print(srcSize)
+				if overwrite == 'file' or 'all':
+					if os.path.isdir(destPath):
+						shutil.rmtree(destPath)
+				if os.path.isdir(srcPath):
+					shutil.copytree(srcPath, destPath)
 				else:
-					shutil.copy(src_path, dest_path)
-			# return build()
-
-		
-				
-				
+					shutil.copy(srcPath, destPath)
+				destSize = getSize(destPath)
+				# print(srcSize, destSize)
+				if srcSize == destSize:
+					# print(workflowFiles.index(file))
+					if workflowFiles.index(file) + 1 ==  len(workflowFiles):
+						copied(overwrite)
+					else:
+						continue
+				else:
+					time.sleep(3)	
---
+++ README.md
@@ -0,0 +1,20 @@
+init-build-tool
+===================================
+前端自動化 sublime text package  
+建立專案自動化環境
+- task
+- package.json
+- build system
+
+## 安裝
+Ctrl+ Shift + p > Add Repository > https://github.com/isobartw-dev/Init-Build-Tool
+
+## 設定
+Preferences > Package Setting > init-build-tool >Settings – Default  
+設定campaign-build-tool的路徑
+```
+"workflowPackagePath":"格式：D:/XXX/XXX"
+```
+## 使用
+- Ctrl+ Shift + p > 建立自動化流程：campign
+- Alt + Shift + i
